289efc44bfec1be674d4c5fca010cdb759ca3751
Serhii Serhieiev <adron@mstnt.com>
2021-04-14 07:31:08 +0300

    first commit

---
+++ Makefile
@@ -0,0 +1,59 @@
+#make по ':all' собирает сразу модули для x86 и mips
+ifndef ARCH
+  export ARCH = x86
+  export KERNEL_DIR = /lib/modules/$(shell uname -r)/build
+endif
+ifeq ($(ARCH),mips)
+ include $(PWD)/lede.mk
+endif
+ifeq ($(ARCH),x86_64)
+ include $(PWD)/lede.mk
+endif
+ifeq ($(ARCH),arm)
+ include $(PWD)/arm.mk
+endif
+ifeq ($(ARCH),arm64)
+ include $(PWD)/arm64.mk
+endif
+
+# standard flags for module builds
+EXTRA_CFLAGS += -DLINUX -D__KERNEL__ -DMODULE -O2 -pipe -Wall
+
+TARGET=test.o
+obj-m:=$(TARGET)
+#test-objs:=test.o
+
+TARGETS := $(obj-m:.o=.ko)
+ccflags-y += -Wall
+
+all:
+#	make x86
+#	make arm
+	make mips
+#	make ramips
+#	make e2k
+
+arch:
+	$(MAKE) -C $(KERNEL_DIR) M=$$PWD
+
+x86:
+	$(MAKE) arch ARCH=x86_64 DEVICE=x86_64
+
+mips:
+	$(MAKE) arch ARCH=mips MANUAL_BUILD=lede-mips DEVICE=rb941
+
+ramips:
+	$(MAKE) arch ARCH=mips MANUAL_BUILD=openwrt-mips DEVICE=digi-ex15
+
+arm:
+	$(MAKE) arch ARCH=arm
+
+kenzo:
+	$(MAKE) arch ARCH=arm64 DEVICE=kenzo
+
+e2k:
+	$(MAKE) arch ARCH=e2k
+
+clean:		
+		rm -f .*.cmd *.mod.c *.ko *.o *~ core $(TARGETS)
+		rm -fr .tmp_versions
---
+++ test.c
@@ -0,0 +1,106 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/gpio.h>
+#include <linux/of.h>
+#include <linux/platform_device.h>
+#include <linux/property.h>
+#include <linux/time.h>
+#include <linux/netdevice.h>
+#include <linux/gpio.h>
+#include <linux/i2c.h>
+#include <linux/phy.h>
+
+struct mii_bus *mdio_i2c_alloc(struct device *parent, struct i2c_adapter *i2c);
+
+static struct i2c_adapter *i2c_adap = NULL;
+static struct i2c_client *i2c_client = NULL;
+struct platform_device *pdev = NULL;
+
+/* SFP modules appear to always have their PHY configured for bus address
+ * 0x56 (which with mdio-i2c, translates to a PHY address of 22).
+ */
+#define SFP_PHY_ADDR	22
+
+static int do_i2c(void){
+	struct i2c_adapter *i2c = i2c_adap;
+	struct mii_bus *i2c_mii;
+	int ret, phy_reg, phy_id1, phy_id2;
+
+	if (!i2c_check_functionality(i2c, I2C_FUNC_I2C))
+		return -EINVAL;
+
+	i2c_mii = mdio_i2c_alloc(&pdev->dev, i2c);
+	if (IS_ERR(i2c_mii))
+		return PTR_ERR(i2c_mii);
+
+	i2c_mii->name = "SFP I2C Bus";
+	i2c_mii->phy_mask = ~0;
+
+	ret = mdiobus_register(i2c_mii);
+	if (ret < 0) {
+		mdiobus_free(i2c_mii);
+		return ret;
+	}
+	printk(KERN_INFO "OWL: %s - done\n", __func__);
+
+	//для начала прочитаем ID-шки phy устройства
+	phy_id1 = mdiobus_read(i2c_mii, SFP_PHY_ADDR, MII_PHYSID1);
+	phy_id2 = mdiobus_read(i2c_mii, SFP_PHY_ADDR, MII_PHYSID2);
+	printk(KERN_INFO "OWL: %s phy ids: %04x:%04x\n", __func__, phy_id1, phy_id2);
+	phy_reg = mdiobus_read(i2c_mii, SFP_PHY_ADDR, MII_BMSR);
+	printk(KERN_INFO "OWL: %s phy BMSR: 0x%08x\n", __func__, phy_reg);
+
+	mdiobus_unregister(i2c_mii);
+	mdiobus_free(i2c_mii);
+
+	return 0;
+}
+
+static int __init
+testdog_init(void){
+	i2c_adap = i2c_get_adapter(0);
+	if (IS_ERR(i2c_adap)) {
+		printk(KERN_WARNING "OWL: Error #1\n");
+		i2c_adap = NULL;
+		i2c_client = NULL;
+		goto end;
+	}
+
+	/* i2c_client = i2c_new_client_device(i2c_adap, &apu4_i2c_board_info);
+	if (IS_ERR(i2c_client)) {
+		printk(KERN_WARNING "OWL: Error #2\n");
+		i2c_client = NULL;
+		goto end;
+	} */
+	printk(KERN_INFO "i2c adapt = 0x%lx is ready\n", (unsigned long)i2c_adap);
+
+	pdev = platform_device_alloc("owl-x991y", -1);
+
+	if (pdev){
+		//platform_device_add_data(pdev, pdata, sizeof(*pdata);
+		platform_device_add(pdev);
+		printk(KERN_INFO "platform device = 0x%lx is ready\n", (unsigned long)pdev);
+		do_i2c();
+	}
+
+end:
+	if(pdev){
+		platform_device_del(pdev);
+		platform_device_put(pdev);
+	}
+	if (i2c_client)
+		i2c_unregister_device(i2c_client);
+	if (i2c_adap)
+		i2c_put_adapter(i2c_adap);
+	return -ENODEV;
+}
+static void __exit
+testdog_exit(void){
+}
+
+module_init(testdog_init);
+module_exit(testdog_exit);
+MODULE_AUTHOR("Serhii Serhieiev <adron@mstnt.com>");
+MODULE_DESCRIPTION("Test driver)");
+MODULE_LICENSE("GPL");
